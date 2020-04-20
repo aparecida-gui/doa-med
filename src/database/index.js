b61045d06969250efa65e775961e5b8da7f5a33d
suele <suelen.ads.guimaraes@gmail.com>
2020-04-19 22:47:34 -0300

    Add verify connection database.

--- src/database/index.js
+++ src/database/index.js
@@ -2,5 +2,17 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/databaseConfig';
 
 const connection = new Sequelize(databaseConfig);
+console.log('file index.js');
+
+connection
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+connection.close();
 
 export default connection;
