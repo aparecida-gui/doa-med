0971ab46785a801f992836cd37c3a7c78821f72c
suele <suelen.ads.guimaraes@gmail.com>
2020-05-05 16:45:13 -0300

    Add access route application.

--- configServer.js
+++ configServer.js
@@ -1,14 +1,25 @@
-import http from 'http';
 import express from 'express';
-import db from './src/database/index';
+import router from './src/back-end/routers';
+import bodyParser from 'body-parser';
+import db from './src/database';
 
-const port = process.env.PORT || 5009;
-const server = http.createServer();
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use(express.json());
+const port = process.env.PORT || 3009;
+
 app.set(db);
+app.use('/', router);
 
-server.listen(port, () => {
-  console.log(`Server running ${port}`);
+app.use((req, res) => {
+  return res
+    .status(404)
+    .json({ message: 'Não foi possível encontrar a página solicitada.' });
 });
+
+app.listen(`${port}`, () => {
+  console.log(`>>>>> server run port: ${port}`);
+});
+
+export default app;
