83e5de41419df04560cb71caa0fbef0ae9fc8bcf
suele <suelen.ads.guimaraes@gmail.com>
2020-05-07 15:06:26 -0300

    Add router register medicine.

--- src/back-end/routers.js
+++ src/back-end/routers.js
@@ -4,5 +4,6 @@ import Medicine from './controller/MedicineController';
 const router = express.Router();
 
 router.get('/medicine/:name', Medicine.medicineSearch);
+router.post('/medicine', Medicine.registerMedicine);
 
 export default router;
