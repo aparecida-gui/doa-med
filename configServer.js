a05a8e38b0fc98ac5c4aa6544a2f94b489ca4c93
suele <suelen.ads.guimaraes@gmail.com>
2020-05-13 17:16:16 -0300

    Add url access files static.

--- configServer.js
+++ configServer.js
@@ -2,12 +2,18 @@ import express from 'express';
 import router from './src/back-end/routers';
 import bodyParser from 'body-parser';
 import db from './src/database';
+import path from 'path';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 7009;
+
+app.use(
+  '/files/',
+  express.static(path.resolve(__dirname, '.', 'tmp', 'uploads'))
+);
 
 app.set(db);
 app.use('/', router);
