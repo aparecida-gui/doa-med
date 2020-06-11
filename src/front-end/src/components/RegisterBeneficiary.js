9e45f5be59f0db15893475813ed0f79c5d005c7d
suele <suelen.ads.guimaraes@gmail.com>
2020-06-10 22:16:08 -0300

    Add message success validate register beneficiary

--- src/front-end/src/components/RegisterBeneficiary.js
+++ src/front-end/src/components/RegisterBeneficiary.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import InputMask from 'react-input-mask';
+import { Link } from 'react-router-dom';
 
 class RegisterBeneficiary extends Component {
   state = {
     name: '',
-    phone: '',
+    phone: React.createRef(),
     city: '',
     email: '',
+    beneficiarioRegister: false,
   };
 
   handleSubmit = async () => {
@@ -22,68 +24,82 @@ class RegisterBeneficiary extends Component {
       'http://localhost:7009/beneficiary/register_beneficiary',
       beneficiario
     );
-    console.log('registerBenef: ', registerBenef);
+
+    if (registerBenef.status === 200) {
+      this.setState({ beneficiarioRegister: true });
+    }
   };
+
   render() {
     return (
-      <form onSubmit={(e) => e.preventDefault()}>
-        <h1 className="text-center">Beneficiario</h1>
-        <div className="form-group">
-          <label htmlFor="name">Nome</label>
-          <input
-            value={this.state.name}
-            onChange={(e) => this.setState({ name: e.target.value })}
-            type="text"
-            className="form-control"
-            id="name"
-            aria-describedby="emailHelp"
-          />
+      <div>
+        <div style={{ paddingTop: ' 4rem' }} className="row">
+          {this.state.beneficiarioRegister === true && (
+            <div className="alert alert-success" role="alert">
+              <h4>Beneficiario cadastrado com sucesso.</h4>
+              Acesse a página <Link to="/">Login</Link>
+            </div>
+          )}
         </div>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <h1 className="text-center">Beneficiario</h1>
+          <div className="form-group">
+            <label htmlFor="name">Nome</label>
+            <input
+              value={this.state.name}
+              onChange={(e) => this.setState({ name: e.target.value })}
+              type="text"
+              className="form-control"
+              id="name"
+              aria-describedby="emailHelp"
+            />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="phone">Telefone</label>
-          <InputMask
-            mask="(99) 99999-9999"
-            onChange={(e) => this.setState({ phone: e.target.value })}
-            value={this.state.phone}
-            type="tel"
-            id="phone"
-            placeholder="Ex.: (00) 0000-0000"
-            className="form-control"
-            aria-describedby="emailHelp"
-          />
-        </div>
+          <div className="form-group">
+            <label htmlFor="phone">Telefone</label>
+            <InputMask
+              mask="(99) 99999-9999"
+              onChange={(e) => this.setState({ phone: e.target.value })}
+              value={this.state.phone}
+              type="tel"
+              id="phone"
+              placeholder="Ex.: (00) 0000-0000"
+              className="form-control"
+              aria-describedby="emailHelp"
+            />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="city">Cidade</label>
-          <input
-            value={this.state.city}
-            onChange={(e) => this.setState({ city: e.target.value })}
-            type="text"
-            className="form-control"
-            id="city"
-          />
-        </div>
+          <div className="form-group">
+            <label htmlFor="city">Cidade</label>
+            <input
+              value={this.state.city}
+              onChange={(e) => this.setState({ city: e.target.value })}
+              type="text"
+              className="form-control"
+              id="city"
+            />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            value={this.state.email}
-            onChange={(e) => this.setState({ email: e.target.value })}
-            type="email"
-            className="form-control"
-            id="email"
-            aria-describedby="emailHelp"
-          />
-        </div>
-        <button
-          onClick={this.handleSubmit}
-          type="submit"
-          className="btn btn-outline-success"
-        >
-          Registrar
-        </button>
-      </form>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+              type="email"
+              className="form-control"
+              id="email"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <button
+            onClick={this.handleSubmit}
+            type="submit"
+            className="btn btn-outline-success"
+          >
+            Registrar
+          </button>
+        </form>
+      </div>
     );
   }
 }
