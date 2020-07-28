4616e67e8ee88b134f089292440a5bd7e7a98682
suele <suelen.ads.guimaraes@gmail.com>
2020-07-28 19:51:10 -0300

    Adjust validation user and user exit in database.

--- src/back-end/controller/RegisterUserController.js
+++ src/back-end/controller/RegisterUserController.js
@@ -2,15 +2,46 @@ import RegisterUserModel from '../model/RegisterUser';
 import encrypData from '../help/encryptData';
 import jwt from '../help/jwt';
 
-class RegisterUser {
-  async register(req, res) {
-    const hashPassword = encrypData.hashPassword(req.body.password);
-    req.body.password = hashPassword;
+const data = (dataRequest) => {
+  if (!dataRequest.name) {
+    return { message: 'O campo nome está Vaziu' };
+  } else if (!dataRequest.phone) {
+    return { message: 'O campo telefone está Vaziu' };
+  } else if (!dataRequest.city) {
+    return { message: 'O campo cidade está Vaziu' };
+  } else if (!dataRequest.email) {
+    return { message: 'O campo email está Vaziu' };
+  } else if (!dataRequest.password) {
+    return { message: 'O campo senha está Vaziu' };
+  } else {
+    return true;
+  }
+};
 
+class RegisterUser {
+  static async userVerify(user) {
+    const verify = await RegisterUserModel.findAll({
+      where: { email: user.email },
+    });
+
+    if (verify.length > 0) {
+      return { message: 'Usuário já cadastrado.' };
+    } else {
+      return true;
+    }
+  }
+
+  async register(req, res) {
     const { name, phone, city, email, password } = req.body;
+    const isUserExit = await RegisterUser.userVerify(req.body);
+    let validData = data(req.body);
+    const token = await jwt.generationToken();
 
     try {
-      if (name && phone && city && email && password) {
+      if (validData === true && isUserExit === true && token.length > 0) {
+        const hashPassword = encrypData.hashPassword(req.body.password);
+        req.body.password = hashPassword;
+
         const user = await RegisterUserModel.create({
           name,
           phone,
@@ -18,17 +49,13 @@ class RegisterUser {
           email,
           password,
         });
-        if (user) {
-          const token = jwt.generationToken();
-          console.log('>>>>>>>>>>>', token);
-        }
+
         res.status(200).json({
           messageSuccess: `Seja bem-vindo(a) ao DoaMed ${name}`,
+          token,
         });
       } else {
-        res.json({
-          message: 'Por Favor verifique se todos os campos estão preenchidos.',
-        });
+        return res.status(400).json({ validData, isUserExit, token });
       }
     } catch (error) {
       res.status(400).json({
