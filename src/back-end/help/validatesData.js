94c702e64d31f6b84219e54837c3c16e058b6601
suele <suelen.ads.guimaraes@gmail.com>
2020-08-01 23:26:48 -0300

    Add validate

--- src/back-end/help/validatesData.js
+++ src/back-end/help/validatesData.js
@@ -1,14 +1,26 @@
 const userDatas = (dataRequest) => {
   if (!dataRequest.name) {
     return { message: 'O campo nome está Vaziu' };
+  } else if (typeof dataRequest.name !== 'string') {
+    return { message: 'O campo nome não aceita números.' };
   } else if (!dataRequest.phone) {
     return { message: 'O campo telefone está Vaziu' };
   } else if (!dataRequest.city) {
     return { message: 'O campo cidade está Vaziu' };
+  } else if (typeof dataRequest.city !== 'string') {
+    return { message: 'O campo cidade não aceita números.' };
   } else if (!dataRequest.email) {
     return { message: 'O campo email está Vaziu' };
+  } else if (typeof dataRequest.email !== 'string') {
+    return { message: 'O campo email não aceita números.' };
   } else if (!dataRequest.password) {
     return { message: 'O campo senha está Vaziu' };
+  } else if (dataRequest.password.length < 6) {
+    return { message: 'A sua senha deve conter no minimo 6 caracteres.' };
+  } else if (dataRequest.password.length > 12) {
+    return {
+      message: 'Senha muito grande tamanho máximo são 12 caracteres',
+    };
   } else {
     return true;
   }
