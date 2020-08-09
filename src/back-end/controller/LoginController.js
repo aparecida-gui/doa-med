b5524b500e88775ed1f9aa6a7439b770fcf76fc3
suele <suelen.ads.guimaraes@gmail.com>
2020-08-09 16:20:29 -0300

    Add generation token .

--- src/back-end/controller/LoginController.js
+++ src/back-end/controller/LoginController.js
@@ -1,33 +1,39 @@
 import RegisterModel from '../model/RegisterUser';
 import validatesData from '../help/validatesData';
 import encryptData from '../help/encryptData';
+import jwt from '../help/jwt';
 
 class LoginController {
   async login(req, res) {
     const validateDataLogin = validatesData.loginData(req.body);
     const { email, password } = req.body;
-    let loginBeneficiary = null;
+    let emailExists = null;
 
     try {
       if (validateDataLogin === true) {
-        loginBeneficiary = await RegisterModel.findOne({
+        emailExists = await RegisterModel.findOne({
           where: { email },
         });
 
-        if (loginBeneficiary) {
+        if (emailExists) {
           const checkData = await encryptData.checkPassword(
             password,
-            loginBeneficiary.password
+            emailExists.password
           );
-          if (checkData.messageSucess) {
-            res.status(200).json({ messageOk: 'Olá seja bem-vindo(a)' });
+          const token = await jwt.generationToken();
+
+          if (checkData.messageSucess && token) {
+            res
+              .status(200)
+              .json({ messageOk: 'Olá seja bem-vindo(a)', token: token });
           } else {
             return res.status(404).json({
               messagePassword: checkData.messageError,
+              messageTokenError: token.message,
             });
           }
         } else {
-          res.status(404).json({ message: 'Usuário não cadastrado.' });
+          res.status(401).json({ message: 'Usuário não cadastrado.' });
         }
       } else {
         res.status(400).json(validateDataLogin);
