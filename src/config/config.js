649666fa4aae99e34875e2299205a12814a91bb4
suele <suelen.ads.guimaraes@gmail.com>
2020-08-14 13:42:22 -0300

    test heroku 01.

---
+++ src/config/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  development: {
+    dialect: 'postgres',
+    host: '127.0.0.1',
+    username: 'postgres',
+    password: 'postgres',
+    database: 'doaMed',
+    define: {
+      timestamps: false,
+      underscored: true,
+    },
+  },
+  production: {
+    dialect: 'postgres',
+    use_env_variable: process.env.DATABASE_URL,
+  },
+};
--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-import databaseConfig from '../config/databaseConfig';
+import databaseConfig from '../config/config';
 import Medicine_Donation from '../back-end/model/Medicine_Donation';
 import Donor from '../back-end/model/Donor';
 import Donor_Medicine from '../back-end/model/Donor_Medicine';
@@ -7,22 +7,44 @@ import Beneficiary from '../back-end/model/Beneficiary';
 import RegisterUser from '../back-end/model/RegisterUser';
 import Medicine_Beneficiary from '../back-end/model/Medicine_Beneficiary';
 import Photo from '../back-end/model/Photo';
+const env = process.env.NODE_ENV || 'development';
 
-const connection = new Sequelize(
-  databaseConfig.database,
-  databaseConfig.username,
-  databaseConfig.password,
-  {
-    host: databaseConfig.host,
-    dialect: databaseConfig.dialect,
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000,
-    },
-    timestamps: false,
-  }
+console.log(process.env.NODE_ENV);
+console.log('>>>>>>>>>>> env: ', env);
+console.log(
+  '>>>>>>>>>>> databaseConfig[env].use_env_variable',
+  databaseConfig[env].use_env_variable
 );
+console.log('>>>>>>>>>>> databaseConfig: ', databaseConfig);
+console.log('>>>>>>>>>>> databaseConfig.production', databaseConfig.production);
+console.log(
+  '>>>>>>>>>>< databaseConfig.use_env_variable: ',
+  databaseConfig.use_env_variable
+);
+
+let connection;
+if (process.env.NODE_ENV === 'production') {
+  connection = new Sequelize(process.env.use_env_variable, databaseConfig);
+  console.log('>>>>>>>>>>>>>>>>>>>> estou no if');
+}
+if (process.env.NODE_ENV === 'development') {
+  connection = new Sequelize(
+    databaseConfig.development.database,
+    databaseConfig.development.username,
+    databaseConfig.development.password,
+    {
+      host: databaseConfig.development.host,
+      dialect: databaseConfig.development.dialect,
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000,
+      },
+      timestamps: false,
+    }
+  );
+  console.log('estou no else');
+}
 
 connection
   .authenticate()
