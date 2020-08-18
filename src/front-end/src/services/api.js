37d05802f0c062ce7e1f47960276a31be4f37483
suele <suelen.ads.guimaraes@gmail.com>
2020-08-18 17:51:07 -0300

    Add deploy heroku

--- src/front-end/src/services/api.js
+++ src/front-end/src/services/api.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import path from 'path';
+const dotenv = require('dotenv');
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
