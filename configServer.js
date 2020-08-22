49664adb377dc452759d003bfd7825b73095fa4a
suele <suelen.ads.guimaraes@gmail.com>
2020-08-22 17:21:52 -0300

    message

--- configServer.js
+++ configServer.js
@@ -9,22 +9,12 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-const port = process.env.PORT || 5000;
+app.use('/', router);
 
 app.use(
   '/files/',
   express.static(path.resolve(__dirname, '.', 'tmp', 'uploads'))
 );
-
-app.use('/', router);
-
-app.use((req, res, next) => {
-  res
-    .status(404)
-    .json({ message: 'Não foi possível encontrar a página solicitada...' });
-  next();
-});
 
 db.sync()
   .then(() => {
@@ -43,6 +33,15 @@ if (process.env.Node_ENV === 'production') {
     );
   });
 }
+
+app.use((req, res, next) => {
+  res
+    .status(404)
+    .json({ message: 'Não foi possível encontrar a página solicitada...' });
+  next();
+});
+
+const port = process.env.PORT || 5000;
 
 const server = () => {
   app.listen(port);
