afac9e566797a1975dd3cc94eefe73014130ec69
suele <suelen.ads.guimaraes@gmail.com>
2020-11-24 20:09:10 -0300

    Separate private routes for file main.js

--- src/front-end/src/App.js
+++ src/front-end/src/App.js
@@ -1,59 +1,46 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { CssBaseline } from '@material-ui/core';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NavDefault from './components/NavDefault';
-import NavUser from './components/NavUser';
 import SearchMedicine from './components/SearchMedicine';
 import Login from './components/Login';
 import RegisterBeneficiary from './components/RegisterBeneficiary';
-import RecordsOptions from './components/RecordsOptions';
-import RegisterMedicineBenef from './components/RegisterMedicineBenef';
 import ShowMedicineBeneficiary from './components/ShowMedicineBeneficiary';
-import ViewMedicinesRegister from './components/ViewMedicinesRegister';
 import MedicineDonation from './components/MedicineDonation';
+import MainPage from './pages/main';
 import Error404 from './components/Error404';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Route
-        render={() => {
-          return localStorage.getItem('tokenUser') ? (
-            <NavUser />
-          ) : (
-            <NavDefault />
-          );
-        }}
-      />
-      <div className="container my-3 py-3">
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/search_medicine" component={SearchMedicine} />
-          <Route exact path="/record_options" component={RecordsOptions} />
-          <Route exact path="/register_user" component={RegisterBeneficiary} />
-          <Route
-            exact
-            path="/:beneficiary_id/view_medicine_register"
-            component={ViewMedicinesRegister}
-          />
-          <Route
-            exact
-            path="/:beneficiary_id/register_medicine_benef"
-            component={RegisterMedicineBenef}
-          />
-          <Route
-            exact
-            path="/show_medicine_benef"
-            component={ShowMedicineBeneficiary}
-          />
-          <Route
-            exact
-            path="/register_medicine/donantion"
-            component={MedicineDonation}
-          />
-          <Route component={Error404} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <Fragment>
+      <CssBaseline />
+      <BrowserRouter>
+        <NavDefault />
+        <div className="container my-3 py-3">
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/search_medicine" component={SearchMedicine} />
+            <Route
+              exact
+              path="/register_user"
+              component={RegisterBeneficiary}
+            />
+            <Route
+              exact
+              path="/show_medicine_benef"
+              component={ShowMedicineBeneficiary}
+            />
+            <Route
+              exact
+              path="/register_medicine/donantion"
+              component={MedicineDonation}
+            />
+            <Route component={MainPage} />
+            <Route component={Error404} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </Fragment>
   );
 };
 
