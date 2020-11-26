94bc0ca45fb93ebf97de2337d96e702a8a08ab86
suele <suelen.ads.guimaraes@gmail.com>
2020-11-26 03:58:58 -0300

    Add all project routes

---
+++ src/front-end/src/Routers.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Error404 from './components/Error404';
+import RegisterBeneficiary from './pages/RegisterBeneficiary';
+import RegisterMedicineBenef from './pages/RegisterMedicineBenef';
+import ViewMedicinesRegister from './pages/ViewMedicinesRegister';
+
+const Routers = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register_user" component={RegisterBeneficiary} />
+        <Route
+          exact
+          path="/:beneficiary_id/view_medicine_register"
+          component={ViewMedicinesRegister}
+        />
+        <Route
+          exact
+          path="/:beneficiary_id/register_medicine_benef"
+          component={RegisterMedicineBenef}
+        />
+        <Route component={Error404} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routers;
