8b67abc1e24206fd200c331d91139173e58a0be4
suele <suelen.ads.guimaraes@gmail.com>
2020-11-28 02:40:14 -0300

    adjust the relationship between tables.

--- src/back-end/database/migration/20200516222310-Donor_Medicine.js
+++ src/back-end/database/migration/20200516222310-Donor_Medicine.js
@@ -14,7 +14,7 @@ module.exports = {
         donor_id: {
           type: Sequelize.INTEGER,
           allowNull: false,
-          references: { model: 'Donor', key: 'id' },
+          references: { model: 'RegisterUser', key: 'id' },
           onUpdate: 'CASCADE',
           onDelete: 'CASCADE',
         },
