aef07454716567e851ecf911c0b351641377c598
suele <suelen.ads.guimaraes@gmail.com>
2020-12-16 17:21:11 -0300

    Refactor code add hook in login.

--- src/front-end/src/pages/Login.js
+++ src/front-end/src/pages/Login.js
@@ -1,162 +1,124 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import '../index.css';
 import { Grid, Button, TextField, Typography } from '@material-ui/core';
-import api from '../services/api';
-import { Redirect } from 'react-router-dom';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import LockOpenIcon from '@material-ui/icons/LockOpen';
-import { Link } from 'react-router-dom';
-import '../index.css';
+import { Link, useHistory } from 'react-router-dom';
+import { AlertError } from '../components/Alert';
+import api from '../services/api';
 
-class Login extends Component {
-  state = {
-    email: '',
-    password: '',
-    isLogin: false,
-    message: '',
-    beneficiary_id: '',
-  };
+function Login() {
+  let [email, setEmail] = useState('');
+  let [password, setPassword] = useState('');
+  let [message, setMessage] = useState('');
+  let [isLogin, setIsLogin] = useState(false);
+  let [beneficiaryId, setBeneficiaryId] = useState('');
+  let history = useHistory();
 
-  handleSubmit = async () => {
-    const login = {
-      email: this.state.email,
-      password: this.state.password,
-    };
-
-    const { email, password } = login;
+  const handleSubmit = async () => {
     let acessoLogin = await api.post('/', {
       email,
       password,
     });
 
-    try {
-      if (acessoLogin.status === 200) {
-        this.setState({
-          isLogin: true,
-          beneficiary_id: acessoLogin.data.emailExists.id,
-        });
-        return localStorage.setItem('tokenUser', acessoLogin.data.token);
-      }
-    } catch (error) {
-      this.setState({ isLogin: false });
-      if (error.response.data.validateDataLogin) {
-        this.setState({
-          message: error.response.data.validateDataLogin,
-        });
-      }
-      if (error.response.data.message) {
-        this.setState({
-          message: error.response.data.message,
-          email: '',
-          password: '',
-        });
-      }
-      if (error.response.data.messageError) {
-        this.setState({
-          message: error.response.data.messageError,
-        });
-      }
-      if (error.response.data.messagePassword) {
-        this.setState({ message: error.response.data.messagePassword });
-      }
+    if (acessoLogin.status === 200) {
+      setIsLogin((isLogin = true));
+      setBeneficiaryId((beneficiaryId = acessoLogin.data.emailExists.id));
+      console.log(
+        'email: ',
+        email,
+        'password: ',
+        password,
+        'beneficiaryId: ',
+        beneficiaryId,
+        'message: ',
+        message,
+        'isLogin: ',
+        isLogin
+      );
+      localStorage.setItem('tokenUser', acessoLogin.data.token);
+      return history.push(`${beneficiaryId}/register_medicine_benef`);
     }
   };
 
-  render() {
-    return (
-      <Fragment>
-        <div>
-          {this.state.isLogin === true && (
-            <div className="alert alert-success" role="alert">
-              <h4 className="text-center">Seja bem-vindo(a) ao DoaMed</h4>
-              {
-                <Redirect
-                  to={`${this.state.beneficiary_id}/register_medicine_benef`}
-                />
-              }
-            </div>
-          )}
+  return (
+    <Fragment>
+      {message && <AlertError msg={message} />}
 
-          {this.state.isLogin === false && (
-            <div className="alert alert-danger" role="alert">
-              <h4 className="text-center">{this.state.message}</h4>
-            </div>
-          )}
-        </div>
-        <Grid
-          className="grid"
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
-        >
-          <h2>Login</h2>
-          <form onSubmit={(e) => e.preventDefault()}>
-            <Grid item>
-              <TextField
-                required
-                autoFocus
-                fullWidth
-                label="Email"
-                type="email"
-                style={{ margin: 18 }}
-                value={this.state.email}
-                onChange={(e) => this.setState({ email: e.target.value })}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="end">
-                      <MailOutlineIcon />
-                    </InputAdornment>
-                  ),
-                }}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                required
-                fullWidth
-                label="Senha"
-                type="password"
-                style={{ margin: 18 }}
-                value={this.state.password}
-                onChange={(e) => this.setState({ password: e.target.value })}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="end">
-                      <LockOpenIcon />
-                    </InputAdornment>
-                  ),
-                }}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-            </Grid>
-            <Grid item>
-              <Button
-                fullWidth
-                variant="outlined"
-                color="primary"
-                type="submit"
-                style={{ margin: 5 }}
-                onClick={this.handleSubmit}
-              >
-                Logar
-              </Button>
-            </Grid>
-            <Grid item>
-              <Typography align="right" variant="caption">
-                <Link to="/register_user">Faça seu cadastro Aqui</Link>
-              </Typography>
-            </Grid>
-          </form>
-        </Grid>
-      </Fragment>
-    );
-  }
+      <Grid
+        className="grid"
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <h2>Login</h2>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <Grid item>
+            <TextField
+              required
+              autoFocus
+              fullWidth
+              label="Email"
+              type="email"
+              style={{ margin: 18 }}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="end">
+                    <MailOutlineIcon />
+                  </InputAdornment>
+                ),
+              }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              required
+              fullWidth
+              label="Senha"
+              type="password"
+              style={{ margin: 18 }}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="end">
+                    <LockOpenIcon />
+                  </InputAdornment>
+                ),
+              }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              fullWidth
+              variant="outlined"
+              color="primary"
+              type="submit"
+              style={{ margin: 5 }}
+              onClick={handleSubmit}
+            >
+              Logar
+            </Button>
+          </Grid>
+          <Grid item>
+            <Typography align="right" variant="caption">
+              <Link to="/register_user">Faça seu cadastro Aqui</Link>
+            </Typography>
+          </Grid>
+        </form>
+      </Grid>
+    </Fragment>
+  );
 }
 
 export default Login;
