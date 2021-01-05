441111c524608e6ffa5f28543c66cf7e3708cad5
suele <suelen.ads.guimaraes@gmail.com>
2021-01-05 20:43:52 -0300

    Add link to page login .

--- src/front-end/src/pages/RegisterBeneficiary.js
+++ src/front-end/src/pages/RegisterBeneficiary.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import '../index.css';
 import api from '../services/api';
-import { Button, Grid, TextField } from '@material-ui/core';
-import { Redirect } from 'react-router-dom';
+import { Button, Grid, TextField, Typography } from '@material-ui/core';
+import { Link, Redirect } from 'react-router-dom';
 import { AlertError } from '../components/Alert';
 
 export default function RegisterBeneficiary() {
@@ -13,58 +13,25 @@ export default function RegisterBeneficiary() {
   let [password, setPassword] = useState('');
   let [isRegisterOk, setIsRegisterOk] = useState(null);
   let [message, setMessage] = useState('');
-  let listaMenssagens = [];
-
-  const validateFields = () => {
-    if (name.length < 4) {
-      listaMenssagens.push('O campo nome deve ter no minimo 4 caracteres.');
-    }
-    if (phone.length < 8) {
-      listaMenssagens.push('O campo telefone deve ter 8 ou 9 caracteres.');
-    }
-
-    if (phone.length > 9) {
-      listaMenssagens.push('O campo telefone deve ter 8 ou 9 caracteres.');
-    }
-
-    if (city.length < 5) {
-      listaMenssagens.push('O campo cidade é pequeno.');
-    }
-    if (password.length < 6) {
-      listaMenssagens.push('O campo senha deve ter no minimo 6 caracteres.');
-    }
-    if (password.length > 20) {
-      listaMenssagens.push(
-        'O campo senha é muito grande no maximo são 20 caracteres.'
-      );
-    }
-    if (listaMenssagens.length > 0) {
-      return listaMenssagens;
-    }
-  };
-
   const handleSubmit = async () => {
-    validateFields();
-    if (listaMenssagens.length === 0) {
-      try {
-        let registerUser = await api.post('register_user', {
-          name,
-          phone,
-          city,
-          email,
-          password,
-        });
+    try {
+      let registerUser = await api.post('register_user', {
+        name,
+        phone,
+        city,
+        email,
+        password,
+      });
 
-        if (registerUser.status === 201) {
-          setIsRegisterOk((isRegisterOk = true));
-        }
-      } catch (error) {
-        if (error.response.data.isUserExit.message) {
-          setIsRegisterOk((isRegisterOk = false));
-          setMessage((message = error.response.data.isUserExit.message));
+      if (registerUser.status === 201) {
+        setIsRegisterOk((isRegisterOk = true));
+      }
+    } catch (error) {
+      if (error.response.data.isUserExit.message) {
+        setIsRegisterOk((isRegisterOk = false));
+        setMessage((message = error.response.data.isUserExit.message));
 
-          getInitialState();
-        }
+        getInitialState();
       }
     }
   };
@@ -79,7 +46,7 @@ export default function RegisterBeneficiary() {
 
   return (
     <>
-      {isRegisterOk === true && <Redirect exact to="/login" />}
+      {isRegisterOk === true && <Redirect exact to="/" />}
       {isRegisterOk === false && <AlertError msg={message} />}
 
       <Grid container direction="column" justify="center" alignItems="center">
@@ -161,6 +128,11 @@ export default function RegisterBeneficiary() {
               Cadastrar
             </Button>
           </div>
+          <Grid item>
+            <Typography align="right" variant="caption">
+              <Link to="/">Faça seu Login aqui</Link>
+            </Typography>
+          </Grid>
         </form>
       </Grid>
     </>
