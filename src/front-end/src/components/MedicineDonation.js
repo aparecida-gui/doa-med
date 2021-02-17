8002cc0f9075fd25b4100b38a87958cdc734ae30
suele <suelen.ads.guimaraes@gmail.com>
2021-02-17 18:07:04 -0300

    Adjust code for version new in the react.

--- src/front-end/src/components/MedicineDonation.js
+++ src/front-end/src/components/MedicineDonation.js
@@ -1,114 +1,116 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import '../index.css';
+import { Button, Grid, TextField } from '@material-ui/core';
 import api from '../services/api';
+import { useAuth } from '../contexts/UserContex';
 
-export default class MedicineDonation extends Component {
-  state = {
-    name: '',
-    laboratory: '',
-    quantity: '',
-    expirationDate: '',
-    successMessage: '',
-    errorMessage: '',
-  };
+export default function MedicineDonation() {
+  const [name, setName] = useState('');
+  const [laboratory, setLaboratory] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [expirationDate, setExpirationDate] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-  handleClick = async () => {
+  let { user } = useAuth();
+
+  const handleClick = async () => {
     const dataMedicineDonation = {
-      name: this.state.name,
-      laboratory: this.state.laboratory,
-      quantity: this.state.quantity,
-      expirationDate: this.state.expirationDate,
+      name,
+      laboratory,
+      quantity,
+      expirationDate,
     };
 
     try {
-      const registerMedicineDonantion = await api.post(
-        '/medicine/register_medicine',
+      let registerMedicineDonantion = await api.post(
+        `register_medicine/donor/${user.id}`,
         dataMedicineDonation
       );
       if (registerMedicineDonantion.status === 201) {
-        this.setState({
-          successMessage: registerMedicineDonantion.data.message,
-        });
+        setSuccessMessage(registerMedicineDonantion.data.message);
+        console.log('>>>>>>...', dataMedicineDonation);
       }
     } catch (error) {
-      this.setState({ errorMessage: error.response.data.error });
+      setErrorMessage(error.response.data.error);
+      console.log('>>>>>>...', errorMessage);
     }
   };
-  render() {
-    return (
-      <div>
-        {this.state.successMessage !== '' && (
-          <div className="alert alert-success" role="alert">
-            <h4 className="text-center">{this.state.successMessage}</h4>
-          </div>
-        )}
-        {this.state.errorMessage !== '' && (
-          <div className="alert alert-danger" role="alert">
-            <h4 className="text-center">{this.state.errorMessage}</h4>
-          </div>
-        )}
-        <h2 className="text-center">Cadastrar Medicamento para Doação</h2>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <div className="form-group">
-            <label htmlFor="name">Nome do Medicamento</label>
-            <input
+  return (
+    <div>
+      {successMessage !== '' && (
+        <div>
+          <h4>{successMessage}</h4>
+        </div>
+      )}
+      {errorMessage !== '' && (
+        <div>
+          <h4>{errorMessage}</h4>
+        </div>
+      )}
+      <Grid container direction="column" justify="center" alignItems="center">
+        <form onSubmit={(e) => e.preventDefault()} className="main register">
+          <h2>Cadastrar Medicamento para Doação</h2>
+          <Grid item>
+            <TextField
               autoFocus
               required
+              fullWidth
+              label="Nome do Medicamento"
               type="text"
-              id="name"
-              className="form-control"
-              value={this.state.name}
-              onChange={(e) => this.setState({ name: e.target.value })}
+              value={name}
+              style={{ margin: 18 }}
+              onChange={(e) => setName(e.target.value)}
               placeholder="Digite o nome do medicamento"
             />
-          </div>
-          <div className="form-group">
-            <label htmlFor="laboratory">Laboratório</label>
-            <input
+          </Grid>
+          <Grid item>
+            <TextField
               required
+              fullWidth
+              label="Laboratório"
               type="text"
-              id="laboratory"
-              className="form-control"
-              value={this.state.laboratory}
-              onChange={(e) => this.setState({ laboratory: e.target.value })}
+              value={laboratory}
+              style={{ margin: 18 }}
+              onChange={(e) => setLaboratory(e.target.value)}
               placeholder="Digite o nome do laboratório"
             />
-          </div>
-          <div className="form-group">
-            <label htmlFor="quantity">Quantidade Disponível para Doação</label>
-            <input
+          </Grid>
+          <Grid item>
+            <TextField
               required
+              fullWidth
               type="number"
-              id="quantity"
-              className="form-control"
-              value={this.state.quantity}
-              onChange={(e) => this.setState({ quantity: e.target.value })}
+              label="Quantidade"
+              value={quantity}
+              style={{ margin: 18 }}
+              onChange={(e) => setQuantity(e.target.value)}
               placeholder="Digite a quantidade do medicamento"
             />
-          </div>
-          <div className="form-group">
-            <label htmlFor="expirationDate">Data de Validade</label>
-            <input
+          </Grid>
+          <Grid item>
+            <TextField
               required
+              fullWidth
               type="date"
-              id="expirationDate"
-              value={this.state.expirationDate}
-              onChange={(e) =>
-                this.setState({ expirationDate: e.target.value })
-              }
-              className="form-control"
+              value={expirationDate}
+              style={{ margin: 18 }}
+              onChange={(e) => setExpirationDate(e.target.value)}
             />
-          </div>
-          <div className="form-group">
-            <button
+          </Grid>
+          <Grid item>
+            <Button
+              fullWidth
+              variant="outlined"
+              color="primary"
               type="submit"
-              onClick={this.handleClick}
-              className="btn btn-primary"
+              onClick={handleClick}
             >
               Registrar Medicamento
-            </button>
-          </div>
+            </Button>
+          </Grid>
         </form>
-      </div>
-    );
-  }
+      </Grid>
+    </div>
+  );
 }
