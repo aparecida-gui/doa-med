51b81c54f39afc1cc96f664eda087caed8c4cb63
suele <suelen.ads.guimaraes@gmail.com>
2021-05-04 19:12:01 -0300

    Add Donation Scheduled.

--- src/front-end/src/components/Donation.js
+++ src/front-end/src/components/Donation.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import api from '../services/api';
 import { useAuth } from '../contexts/UserContex';
-import Typography from '@material-ui/core/Typography';
+import { Button, Typography } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
+import moment from 'moment';
+import LayoutPrivate from '../layouts/LayoutPrivate';
 
-export default function RecipeReviewCard() {
+export default function Donation() {
   const [donationScheduled, setDonationScheduled] = useState([]);
   const [message, setMessage] = useState('');
   let { user } = useAuth();
@@ -16,8 +18,9 @@ export default function RecipeReviewCard() {
 
       if (getData.status === 200) {
         setDonationScheduled(getData.data.medicinesScheduledDonation);
-      } else {
-        setMessage(getData.data);
+      }
+      if (getData.data.message) {
+        setMessage(getData.data.message);
       }
     } catch (error) {
       if (error.response === undefined) {
@@ -27,24 +30,63 @@ export default function RecipeReviewCard() {
     }
   };
 
+  useEffect(() => {
+    getDonations();
+  }, []);
+
   return (
-    <Paper elevation={8} style={{ marginTop: 30 }}>
-      <div style={{ margin: 15, padding: 10 }}>
-        <Typography variant="h5">Nome do medicamento</Typography>
-        <Typography variant="subtitle1">Hoje</Typography>
-        <Typography variant="body1">Quantidade</Typography>
-        <Typography variant="body1">Data</Typography>
-        <Typography variant="body1">Hora</Typography>
-        <div style={{ marginTop: 7, paddingTop: 14 }}>
-          <Typography variant="subtitle1">
-            Este medicamento foi doado?
-          </Typography>
-          <div style={{ marginBottom: 8, paddingBottom: 16 }}>
-            <button>Sim</button>
-            <button>Não</button>
-          </div>
-        </div>
-      </div>
-    </Paper>
+    <LayoutPrivate>
+      {donationScheduled &&
+        donationScheduled.map((donation) => (
+          <Paper
+            elevation={8}
+            style={{ marginTop: 30, background: ' #a385cf', color: 'white' }}
+            key={donation.id}
+          >
+            <div style={{ margin: 15, padding: 10 }}>
+              <Typography
+                style={{
+                  fontWeight: 900,
+                  marginBottom: 5,
+                  paddingBottom: 5,
+                  fontSize: 40,
+                }}
+              >
+                Medicamento {donation.name}
+              </Typography>
+              <Typography variant="body1" style={{ fontWeight: 900 }}>
+                Quantidade doada {donation.quantity}
+              </Typography>
+              <Typography variant="body1" style={{ fontWeight: 900 }}>
+                Dia da doação {moment(donation.date).format('DD/MM/YYYY')}
+              </Typography>
+              <Typography variant="body1" style={{ fontWeight: 900 }}>
+                Hora {donation.time}
+              </Typography>
+              <div style={{ marginTop: 7, paddingTop: 14 }}>
+                <Typography variant="subtitle1" style={{ fontWeight: 900 }}>
+                  Este medicamento foi doado?
+                </Typography>
+                <div
+                  style={{
+                    marginTop: 7,
+                    marginBottom: 8,
+                    paddingBottom: 16,
+                    fontWeight: 900,
+                  }}
+                >
+                  <Button variant="contained" color="primary">
+                    Sim
+                  </Button>
+                  <Button variant="contained" color="secondary">
+                    Não
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </Paper>
+        ))}
+      {message && <Typography variant="h4">{message}</Typography>}
+    </LayoutPrivate>
   );
 }
