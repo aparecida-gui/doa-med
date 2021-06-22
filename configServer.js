1545d25f460416aa63ab65a3ebee6c0aab67ead9
suele <suelen.ads.guimaraes@gmail.com>
2021-06-22 16:24:30 -0300

    Add imports ES6.

--- configServer.js
+++ configServer.js
@@ -1,38 +1,14 @@
 import cors from 'cors';
 import express from 'express';
 import router from './src/back-end/routers.js';
-import bodyParser from 'body-parser';
 import db from './src/database/index.js';
 import path from 'path';
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//Quando for fazer deploy comenta está linha das rotas.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
-
-// app.use(
-//   '/files/',
-//   express.static(path.resolve(__dirname, '.', 'tmp', 'uploads'))
-// );
-
-const server = () => {
-  console.log('>>>>>> process.env.PORT: ', process.env.PORT);
-  app.listen(process.env.PORT || 5000);
-  console.log('>>>>> server run');
-};
-
-db.sync()
-  .then(() => {
-    console.log('>>>>>> process.env.PORT: ', process.env.PORT);
-    app.listen(process.env.PORT || 5000);
-    console.log('>>>>> server run');
-  })
-  .catch((err) => {
-    console.log('>>>> Err database : ', err);
-  });
 
 app.use(express.static(path.resolve('./src/front-end/build')));
 
@@ -45,6 +21,16 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join('front-end/build', 'index.html'));
   });
 }
+
+db.sync()
+  .then(() => {
+    console.log('>>>>>> process.env.PORT: ', process.env.PORT);
+    console.log('>>>>> server run');
+    app.listen(process.env.PORT || 5000);
+  })
+  .catch((err) => {
+    console.log('>>>> Err database : ', err);
+  });
 
 app.use((req, res, next) => {
   res
