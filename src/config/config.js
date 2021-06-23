21a5c8fd13cbd6eba03199d411770e3e5fe4f238
suele <suelen.ads.guimaraes@gmail.com>
2021-06-23 01:13:37 -0300

    Add configurations access database.

--- src/config/config.js
+++ src/config/config.js
@@ -13,5 +13,10 @@ export default {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: 'false',
+      },
+    },
   },
 };
