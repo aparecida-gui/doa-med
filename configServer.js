3cc1e39f122594b74c39c0ba406aa812ebb7fc9e
suele <suelen.ads.guimaraes@gmail.com>
2021-06-28 22:33:58 -0300

    Adjust server.

--- configServer.js
+++ configServer.js
@@ -4,12 +4,20 @@ import router from './src/back-end/routers.js';
 import db from './src/database/index.js';
 import path from 'path';
 
-const app = express();
-app.use(cors());
 const port = process.env.PORT || 5000;
-app.use('/', router);
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/', router);
+
+db.sync()
+  .then(() => {
+    server();
+  })
+  .catch((err) => {
+    console.log('>>>> Err database : ', err);
+  });
 
 app.use(express.static(path.resolve('./src/front-end/build')));
 
@@ -22,14 +30,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join('front-end/build', 'index.html'));
   });
 }
-
-db.sync()
-  .then(() => {
-    server();
-  })
-  .catch((err) => {
-    console.log('>>>> Err database : ', err);
-  });
 
 app.use((req, res, next) => {
   res
