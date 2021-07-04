8de9fa7cc73e21454446086c767cc77f28d7f063
suele <suelen.ads.guimaraes@gmail.com>
2021-07-04 01:10:32 -0300

    Adjust cors.

--- configServer.js
+++ configServer.js
@@ -6,9 +6,9 @@ import path from 'path';
 
 const port = process.env.PORT || 5000;
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use('/', router);
 
 db.sync()
